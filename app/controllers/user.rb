943721c54c1333fe7ac52dc246bc8c528bb9c7fe
boguth <bo.guthrie.mail@gmail.com>
2015-06-22 20:25:41 -0500

    Got login working.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,3 +2,13 @@ get '/users/login' do
 
   erb :"/users/login"
 end
+
+post '/users/login' do
+  if User.authenticate(params[:username], params[:password])
+     @user = User.find_by(username: params[:username])
+     redirect '/'
+  else
+    @error = "Please enter a valid username and password."
+    erb :"/users/login"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   end
 
    def self.authenticate(username, password)
-    user = User.find_by(username: username)
+    user = User.find_by(:username => username)
     if user && user.password == password
       return user
     else
